0c3dd4683be2c126fb3f1b7aa4e47035650610ae
samuel-jones-uoit <samuel.jones1@ontariotechu.net>
2022-03-17 13:05:16 -0400

    Board outline
    Some work on the outline of the board nothing final

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,17 @@ $(document).ready(function(){
 });
 
 
-function makeDocument(){
+function makeDocument(){;
 	let gameDiv = document.getElementById("gameDiv");
 	let gameBoard = document.createElement("table");
-	// TODO: Create board
+	gameDiv.appendChild(gameBoard);
+	for (let row = 0; row < BOARD_SIZE; row++){
+		let gameRow = document.createElement("tr");
+		gameBoard.appendChild(gameRow);
+		for (let col = 0; col < BOARD_SIZE; col++){
+			let gameSquare = document.createElement("td");
+			gameRow.appendChild(gameSquare);
+			gameSquare.setAttribute("class", "gamesquare");
+		}
+	}
 }
