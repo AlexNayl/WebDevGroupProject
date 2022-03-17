4abe00ad298554c5777305d9ee47789c2b5446ad
Samuel Jones <samuel.jones1@ontariotechu.net>
2022-03-17 12:12:33 -0400

    Started working on the board

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+const BOARD_SIZE = 9; // Can change later this represents an 8x8 board
+
+// Definite Start-Up procedure
+$(document).ready(function(){
+	makeDocument();
+});
+
+
+function makeDocument(){
+	let gameDiv = document.getElementById("gameDiv");
+	let gameBoard = document.createElement("table");
+	// TODO: Create board
+}
