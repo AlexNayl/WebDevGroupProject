99ccbfabb28c8c9733535ca9fdda3dcf0d1a7655
samuel-jones-uoit <samuel.jones1@ontariotechu.net>
2022-03-25 14:27:57 -0400

    Some Minesweeper UI progress

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,23 @@ const BOARD_SIZE = 9; // Can change later this represents an 8x8 board
 // Definite Start-Up procedure
 $(document).ready(function(){
 	makeDocument();
+
+	// Disable right click browser menu on TD
+	$("td").contextmenu(function(){
+		return false;
+	});
+
+	// Set up left and right click events
+	$("td").mousedown(function(event){
+		console.log("click");
+		if (event.which == 1){ // if left click
+			clickSquare(this);
+		}else if (event.which == 3){ // if right click
+			rightClickSquare(this);
+		}
+	});
+
+
 });
 
 
@@ -16,7 +33,37 @@ function makeDocument(){;
 		for (let col = 0; col < BOARD_SIZE; col++){
 			let gameSquare = document.createElement("td");
 			gameRow.appendChild(gameSquare);
-			gameSquare.setAttribute("class", "gamesquare");
+			gameSquare.setAttribute("class", "gamesquare unknownsquare");
 		}
+	}
+}
+
+function inArray(array, element){
+	for (let i = 0; i < array.length; i++){
+		if (element == array[i]){
+			return true;
+		}
+	}
+	return false;
+}
+
+function clickSquare(square){
+	if (!inArray(square.classList, "unknownsquare")){
+		return;
+	}
+	square.classList.remove("unknownsquare");
+	// temp
+	square.classList.add("emptysquare");
+}
+
+function rightClickSquare(square){
+	if (inArray(square.classList, "unknownsquare")){
+		square.classList.add("flagsquare");
+		square.classList.remove("unknownsquare");
+		// TODO: mark(flagged)
+	}else if(inArray(square.classList, "flagsquare")){
+		// TODO: mark(not flagged)
+		square.classList.remove("flagsquare");
+		square.classList.add("unknownsquare");
 	}
 }
