03438b52127d29ed552e15505eafb59d9e2d3177
samuel-jones-uoit <samuel.jones1@ontariotechu.net>
2022-03-25 17:01:01 -0400

    Created game class

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,61 @@
-const BOARD_SIZE = 9; // Can change later this represents an 8x8 board
+var game;
+class Minesweeper{
+	constructor(BOARD_SIZE){
+		this.BOARD_SIZE = BOARD_SIZE;
+		this.squares = [];
+		this.setup();
+	}
 
+	setup(){
+		// Setup Board
+		let board = document.getElementById("gameBoard");
+		// if already exists
+		if (board){
+			board.remove();
+		}
+		board = this.makeBoard();
+		//Events
+
+		// Disable right click browser menu on game squares
+		$(".gamesquare").contextmenu(function(){
+			return false;
+		});
+
+		// Set up left and right click events
+		$(".gamesquare").mousedown(function(event){
+			if (event.which == 1){ // if left click
+				clickSquare(this);
+			}else if (event.which == 3){ // if right click
+				rightClickSquare(this);
+			}
+		});
+	}
+
+	makeBoard(){
+		let gameDiv = document.getElementById("gameDiv");
+		let gameBoard = document.createElement("table");
+		gameBoard.setAttribute("id", "gameBoard");
+		gameDiv.appendChild(gameBoard);
+		for (let row = 0; row < this.BOARD_SIZE; row++){
+			let gameRow = document.createElement("tr");
+			gameBoard.appendChild(gameRow);
+			this.squares.push([]);
+			for (let col = 0; col < this.BOARD_SIZE; col++){
+				let gameSquare = document.createElement("td");
+				gameRow.appendChild(gameSquare);
+				gameSquare.setAttribute("class", "gamesquare unknownsquare");
+				this.squares[row].push(gameSquare);
+			}
+		}
+		return gameBoard;
+	}	
+}
 // Definite Start-Up procedure
 $(document).ready(function(){
-	makeDocument();
-
-	// Disable right click browser menu on TD
-	$("td").contextmenu(function(){
-		return false;
-	});
-
-	// Set up left and right click events
-	$("td").mousedown(function(event){
-		console.log("click");
-		if (event.which == 1){ // if left click
-			clickSquare(this);
-		}else if (event.which == 3){ // if right click
-			rightClickSquare(this);
-		}
-	});
-
-
+	// Set up Game
+	const BOARD_SIZE = 9; // Can change later this represents an 9x9 board
+	game = new Minesweeper(BOARD_SIZE);
 });
-
-
-function makeDocument(){;
-	let gameDiv = document.getElementById("gameDiv");
-	let gameBoard = document.createElement("table");
-	gameDiv.appendChild(gameBoard);
-	for (let row = 0; row < BOARD_SIZE; row++){
-		let gameRow = document.createElement("tr");
-		gameBoard.appendChild(gameRow);
-		for (let col = 0; col < BOARD_SIZE; col++){
-			let gameSquare = document.createElement("td");
-			gameRow.appendChild(gameSquare);
-			gameSquare.setAttribute("class", "gamesquare unknownsquare");
-		}
-	}
-}
 
 function inArray(array, element){
 	for (let i = 0; i < array.length; i++){
@@ -52,17 +71,15 @@ function clickSquare(square){
 		return;
 	}
 	square.classList.remove("unknownsquare");
-	// temp
-	square.classList.add("emptysquare");
+	// TODO: Take actions in game based on the click
+	square.classList.add("emptysquare"); // temp
 }
 
 function rightClickSquare(square){
 	if (inArray(square.classList, "unknownsquare")){
 		square.classList.add("flagsquare");
 		square.classList.remove("unknownsquare");
-		// TODO: mark(flagged)
 	}else if(inArray(square.classList, "flagsquare")){
-		// TODO: mark(not flagged)
 		square.classList.remove("flagsquare");
 		square.classList.add("unknownsquare");
 	}
