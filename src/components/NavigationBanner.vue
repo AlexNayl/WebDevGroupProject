c9c3d46b120be18e2e27712a81e3a2181698c215
BenceTakacs15 <bence.takacs@ontariotechu.net>
2022-04-26 19:55:40 -0400

    Fixed nav bar order

--- src/components/NavigationBanner.vue
+++ src/components/NavigationBanner.vue
@@ -4,10 +4,10 @@
             <router-link to="/">Home</router-link> |
             <router-link to="/MineSweeper">MineSweeper</router-link> |
 			<router-link to="/Snake">Snake</router-link> |
-            <router-link to="/Highscores">Highscores</router-link> |
+            <router-link to="/WordSearch">WordSearch</router-link> |
 			<router-link to="/Stacker">Stacker</router-link> |
-            <router-link to="/ScoreVisualization">Graphs</router-link> |
-            <router-link to="/WordSearch">WordSearch</router-link>
+            <router-link to="/Highscores">Highscores</router-link> |
+            <router-link to="/ScoreVisualization">Graphs</router-link>
         </nav>
     </header>
 </template>
