167af1eabc088ee3301bfa8f39ae42bc7bc28bb6
ZacharyWindover <71024596+ZacharyWindover@users.noreply.github.com>
2022-04-26 16:35:36 -0400

    Create WordSearch.vue

---
+++ src/views/WordSearch.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="WordSearch">
+		<h1>Word Search</h1>
+		<WordSearchGame />
+	</div>
+</template>
+
+<script>
+	import WordSearchGame from '@/components/WordSearchGame.vue'
+	export default {
+		name: "WordSearch",
+		components: {
+			WordSearchGame,
+		},
+	}
+</script>
